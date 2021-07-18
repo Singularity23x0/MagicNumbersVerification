62d8134ed5d4ca4a4e6818b155c291031e2d0773
Singularity23x0 <dawidpastuszka23x0@gmail.com>
2021-07-18 15:31:23 +0200

    First version of the full implementation.

--- src/main/java/DataTypeAnalisys/FileVerifier.java
+++ src/main/java/DataTypeAnalisys/FileVerifier.java
@@ -5,15 +5,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class FileVerifier {
+
     public void verifyFileExtension(String[] filesPaths) throws Exception {
         for (String path : filesPaths
              ) {
-            File file = new File(path);
-            DataType fileIntroducedAs = DataType.getTypeOf(path);
-            if(fileIntroducedAs == DataType.UNDEFINED) {
-                throw new Exception("The program did not recognize this file's extention");
-            } else {
+            this.analyzeFile(path);
+        }
+    }
 
+    private void analyzeFile(String path) throws Exception {
+        File file = new File(path);
+        DataType fileIntroducedAs = DataType.getTypeOf(path);
+        if(fileIntroducedAs == DataType.UNDEFINED) {
+            throw new Exception("The program did not recognize this file's extention");
+        } else {
+            DataTypeAnalyzer analyzer = new DataTypeAnalyzer(fileIntroducedAs);
+            boolean isClaimedType = analyzer.isType(file);
+            if(isClaimedType){
+                System.out.println("This file is a: " + fileIntroducedAs.toString());
+            } else {
+                throw new Exception("File introduced as: " + fileIntroducedAs.toString() + ", but magic numbers proved that it is not true!");
             }
         }
     }
